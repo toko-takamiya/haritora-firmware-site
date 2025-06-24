10789252bc317420b05822694e8b56cf9cb96465
Maya <jovannmc@femboyfurry.net>
2025-06-24 21:37:14 +0300

    feat: init skeleton/sveltekit project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		{@render children()}
+	</main>
+
+	<Footer />
+</div>
+
+<style>
+
+</style>
