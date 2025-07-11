498d232ee3a3ccca786284728941d3790aff13d6
Maya <jovannmc@femboyfurry.net>
2025-07-11 11:36:26 +0300

    feat: add new languages

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <footer class="w-full bg-gray-900 py-4 text-center text-gray-200">
-	<div class="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+	<div class="container mx-auto flex flex-col items-center justify-between gap-4 px-4 lg:flex-row">
 		<div class="text-sm opacity-80">
 			&copy; {new Date().getFullYear()} JovannMC. All rights reserved. All wrongs unreserved. :3
 		</div>
@@ -21,8 +21,9 @@
 				}
 			}}
 		>
-			<option value="en">English</option>
-			<!-- <option value="ja">日本語</option> -->
+			<option value="en">English (World)</option>
+			<option value="ja">Japanese (日本語)</option>
+			<option value="pt-br">Português (Brasil)</option>
 		</select>
 	</div>
 </footer>
